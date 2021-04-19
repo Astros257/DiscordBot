0c07fe3bd4375f4c93835f3cb44170ef5095a306
Astros257 <astros257@gmail.com>
2021-04-19 02:01:39 -0500

    Added discriptive comments to what the events do and fixed some bugs with joining channles

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const {OpusEncoder} = require('@discordjs/opus')
+const { OpusEncoder } = require("@discordjs/opus");
 
 const client = new Discord.Client();
 const guild = client.guild;
-//const member = guild.members.cache.get("UserID");
 
-var user = [];
-var botTextChannel;
+var user = []; //storing our users here
+var botTextChannel = null; //text channel we will use to write messages too
 
 client.on("ready", () => {
   //once its ready it will call the callback function
@@ -17,6 +16,7 @@ client.on("ready", () => {
 //hide the token
 client.login(process.env.TOKEN);
 
+//We handle Messages like commands or we look for a particular pharase
 client.on("message", async (msg) => {
   //once we receive a message we will reply wiht pong
   if (msg.content === "!guild") {
@@ -25,14 +25,16 @@ client.on("message", async (msg) => {
 
   if (msg.author.bot) return;
 
+  //Helps the user by letting them know of the commands
   if (msg.content === "!assit") {
     msg.reply(
       "Yes Hello I am a bot that will play an intro song for you \n" +
-      "$set: will include you one my stalker list \n" +
-      "$settc: will set the text channel where I will send messages to"
+        "$set: will include you one my stalker list \n" +
+        "$settc: will set the text channel where I will send messages to"
     );
   }
 
+  //Test Message
   if (msg.content === "your a bot") {
     if (msg.member.voice.channel) {
       const conneection = await msg.member.voice.channel.join();
@@ -42,69 +44,60 @@ client.on("message", async (msg) => {
     }
   }
 
-  if(msg.content === "$set"){
-    let curUser = msg.member.user
-    if(!user.includes(curUser))
-      user.push(curUser)
+  //Sets the user who initiated the command to be tracked by the bot
+  if (msg.content === "$set") {
+    let curUser = msg.member.user;
+    if (!user.includes(curUser)) user.push(curUser);
     else
-      user.forEach(element => {
-        msg.reply(`${element} is in the DB`)
-        
+      user.forEach((element) => {
+        msg.reply(`${element} is in the DB`);
       });
   }
-
-  if(msg.content === '$settc'){
+  //sets the text channel for our bot to send messages int
+  if (msg.content === "$settc") {
     botTextChannel = msg.channel;
     msg.reply(`Channel ID set to ${botTextChannel}`);
-   // console.log(botTextChannel);
+    // console.log(botTextChannel);
   }
 });
 
-// client.on('voiceStateUpdate', async (oldState, newState) => {
-//   let newUserChannel = newState.voiceChannel;
-//   let oldUserChannel = oldState.voiceChannel;
-//   console.log(`${newUserChannel}  ${oldUserChannel}`);
-
-//   if (oldUserChannel === undefined && newUserChannel !== undefined) {
-//     //user joines  a voice channel
-//     const conneection = newUserChannel.join(); //msg.member.voice.channel.join()
-//     client.reply("someone joined");
-//   } else if (newUserChannel === undefined) {
-//     //user leavesa a voice channel
-//   }
-// });
-
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-  let newchannelID = newMember.channelID
-  let oldChannelID = oldMember.channelID
-  let newUserChannel = client.channels.cache.get(newchannelID)// newMember.channelID;
-  let oldUserChannel = client.channels.cache.get(oldChannelID)// newMember.channelID;
+//function handles users leaving and entering a voice channel
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+  let newchannelID = newMember.channelID;
+  let oldChannelID = oldMember.channelID;
+  let newUserChannel = client.channels.cache.get(newchannelID); // newMember.channelID;
+  let oldUserChannel = client.channels.cache.get(oldChannelID); // newMember.channelID;
 
   //console.log(`NewMember: ${newUserChannel} oldMember: ${oldUserChannel}`);
- // console.log(newMember);
+  //console.log(newMember);
 
-  
-  if(oldUserChannel === undefined && newUserChannel !== undefined) //don't remove ""
-  { 
-    // User Joins a voice channel
-    //console.clear();
-    //console.log("Joined vc with id "+newUserChannel);
-    console.log(`${newUserChannel}  ${oldUserChannel}`);
+  if (oldUserChannel === undefined && newUserChannel !== undefined) {
+    //user joined voice channel
+    console.log("Joined vc with user " + newMember.member.user.username);
+
+    //join the voice channel
     const connection = newUserChannel.join();
-    if(botTextChannel !== '' || botTextChannel !== undefined){
-      if(botTextChannel != null){
-        client.channels.cache.get(botTextChannel.id).send(`${newMember.member.user} has joned: ${newUserChannel}`);
-      }
-      else
-      {
-        console.log("No Voice Channel is set");
-      }
+
+    console.log(
+      newMember.member.user.username + " Has Joined " + newMember.channel.name
+    );
+    if (botTextChannel != null && botTextChannel != "") {
+      client.channels.cache
+        .get(botTextChannel.id)
+        .send(`${newMember.member.user} has joned: ${newUserChannel}`);
+    } else {
+      console.log("No Text Channel is set");
     }
-      
-  }
-  else if(newUserChannel === undefined){
-      // User leaves a voice channel
-      console.log("Left vc");
-      client.channels.cache.get(botTextChannel.id).send(`${oldMember.member.user} has left: ${oldUserChannel}`);
+  } //user left voice channel
+  else if (newUserChannel === undefined) {
+    // User leaves a voice channel
+    console.log(
+      oldMember.member.user.username + " Has Left  " + oldMember.channel.name
+    );
+    if (botTextChannel != null && botTextChannel != "") {
+      client.channels.cache
+        .get(botTextChannel.id)
+        .send(`${oldMember.member.user} has left: ${oldUserChannel}`);
+    }
   }
 });
